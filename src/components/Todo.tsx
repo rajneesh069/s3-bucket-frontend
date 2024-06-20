e92b0a322238c748ea90da240408d39b5390d644
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-20 12:12:07 +0530

    add todo app

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,37 @@
-export default function Todo() {}
+import { generateTodos, Todo as TodoType } from "../utils/todoGenerator";
 
-export function TodoCard() {
-
-    return <>
-    
+export default function Todo() {
+  const todos: TodoType[] = generateTodos();
+  return (
+    <>
+      {todos.map((todo: TodoType) => (
+        <TodoCard title={todo.title} description={todo.description} />
+      ))}
     </>
+  );
+}
+
+export function TodoCard({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <>
+      <div
+        style={{
+          border: "1px solid black",
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+          marginBottom: 5,
+        }}
+      >
+        <p>Title : {title}</p>
+        <p>Description : {description}</p>
+      </div>
+    </>
+  );
 }
